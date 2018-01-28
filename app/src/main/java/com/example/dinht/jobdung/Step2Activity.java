5a0a883826225f975bd2e298e078bedc544e9e38
dinhtienduc <dinhtienduc93@gmail.com>
2018-01-28 23:20:37 +0700

    project

---
+++ app/src/main/java/com/example/dinht/jobdung/Step2Activity.java
@@ -0,0 +1,13 @@
+package com.example.dinht.jobdung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Step2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step2);
+    }
+}
---
+++ app/src/main/java/com/example/dinht/jobdung/inforPayActivity.java
@@ -0,0 +1,13 @@
+package com.example.dinht.jobdung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inforPayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infor_pay);
+    }
+}
